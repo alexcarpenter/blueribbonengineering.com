231f8cdcbdd9d85cc587969a46766b9d29b9bf30
Alex Carpenter <im.alexcarpenter@gmail.com>
2024-12-02 19:34:41 -0500

    naming

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
 export default function Home() {
   return (
-    <h1 className="uppercase text-avocado-600">
+    <h1 className="uppercase">
       Blue Ribbon
       <br />
-      Engineering
+      Engineering_
     </h1>
   );
 }
