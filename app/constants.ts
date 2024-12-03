e6b2459c41b93e33ccc305f7dd7507cf826bb921
Alex Carpenter <im.alexcarpenter@gmail.com>
2024-12-02 19:17:54 -0500

    tailwindcss v4

---
+++ app/constants.ts
@@ -0,0 +1,2 @@
+export const SITE_TITLE = "Blue Ribbon Engineering";
+export const SITE_DESCRIPTION = "by Alex Carpenter";
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <h1 className="uppercase">
+    <h1 className="uppercase text-avocado-600">
       Blue Ribbon
       <br />
       Engineering
