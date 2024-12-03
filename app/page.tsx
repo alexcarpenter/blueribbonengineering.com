60b55d0f0a507e9961aedb0e6f23c4743f36be1f
Alex Carpenter <im.alexcarpenter@gmail.com>
2024-12-02 21:08:30 -0500

    version

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,31 @@
-export default function Home() {
+import { readFile } from "fs/promises";
+import path from "path";
+
+async function getPackageVersion() {
+  try {
+    const packageJsonPath = path.join(process.cwd(), "package.json");
+    const fileContents = await readFile(packageJsonPath, "utf8");
+    const packageJson = JSON.parse(fileContents);
+    return packageJson.version;
+  } catch (error) {
+    console.error("Could not read package.json", error);
+    return "Unknown";
+  }
+}
+
+export default async function Home() {
+  const version = await getPackageVersion();
+
   return (
     <>
-      <h1 className="uppercase">
-        Blue Ribbon
-        <br />
-        Engineering
-      </h1>
+      <header className="flex justify-between items-end gap-x-4">
+        <h1 className="uppercase">
+          Blue Ribbon
+          <br />
+          Engineering
+        </h1>
+        <p className="text-neutral-500">v{version}</p>
+      </header>
     </>
   );
 }
