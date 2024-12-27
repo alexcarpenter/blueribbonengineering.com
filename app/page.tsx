828aa0391aae958ee9079a076f9ed15a6d86aea1
Alex Carpenter <im.alexcarpenter@gmail.com>
2024-12-27 14:32:14 -0500

    gaps

--- app/page.tsx
+++ app/page.tsx
@@ -3,9 +3,11 @@ import { Heading } from "@/components/heading";
 export default async function Home() {
   return (
     <>
-      <p className="mb-8 uppercase italic">
-        // Exploring engineering excellence
-      </p>
+      <header>
+        <h2 className="uppercase italic">
+          // Exploring engineering excellence
+        </h2>
+      </header>
 
       {/* <section className="mt-8">
         <Heading>Prototypes</Heading>
--- components/footer.tsx
+++ components/footer.tsx
@@ -24,13 +24,13 @@ const items = [
 export function Footer() {
   return (
     <footer>
-      <dl className="grid grid-cols-1 sm:grid-cols-[50%_1fr] gap-x-2 gap-y-2 uppercase">
+      <dl className="grid grid-cols-1 sm:grid-cols-[50%_1fr] gap-x-[1ch] gap-y-2 uppercase">
         {items.map(({ title, description }) => {
           return (
             <React.Fragment key={title}>
               <dt
                 className={cx(
-                  "flex items-center gap-x-2",
+                  "flex items-center gap-x-[1ch]",
                   "after:h-px after:min-w-8 after:border-t after:flex-1"
                 )}
               >
