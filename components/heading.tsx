929db44427242d15bf9e521c862193e73b755c74
Alex Carpenter <im.alexcarpenter@gmail.com>
2024-12-27 14:41:43 -0500

    fix

--- components/heading.tsx
+++ components/heading.tsx
@@ -1,9 +1,9 @@
 export function Heading({ children }: { children: React.ReactNode }) {
   return (
     <h2 className="flex items-center gap-x-[1ch] italic uppercase">
-      <span aria-hidden>/*</span>
+      <span aria-hidden>{"/*"}</span>
       {children} <span aria-hidden className="h-px flex-1 border-t" />
-      <span aria-hidden>*/</span>
+      <span aria-hidden>{"*/"}</span>
     </h2>
   );
 }
