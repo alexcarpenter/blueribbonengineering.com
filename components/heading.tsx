f492479804441ca6aac0ab9a794a4d27e652dbdb
Alex Carpenter <im.alexcarpenter@gmail.com>
2025-01-01 12:44:54 -0500

    astro

--- components/heading.tsx
+++
@@ -1,9 +0,0 @@
-export function Heading({ children }: { children: React.ReactNode }) {
-  return (
-    <h2 className="flex items-center gap-x-[1ch] uppercase">
-      <span aria-hidden>{"/*"}</span>
-      {children} <span aria-hidden className="h-px flex-1 border-t" />
-      <span aria-hidden>{"*/"}</span>
-    </h2>
-  );
-}
--- mdx-components.tsx
+++
@@ -1,9 +0,0 @@
-import type { MDXComponents } from "mdx/types";
-import { Code as code } from "@/components/mdx/code";
-
-export function useMDXComponents(components: MDXComponents): MDXComponents {
-  return {
-    ...components,
-    code,
-  };
-}
